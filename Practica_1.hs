e658ef14de53346617ff77f4f1ec95c9a08fa3a9
BecVal <cesarbecval@ciencias.unam.mx>
2026-02-27 17:56:29 -0600

    Practica 1 inicializada

---
+++ Practica_1.hs
@@ -0,0 +1,23 @@
+import Text.XHtml (rev)
+{-
+    Práctica 1 - Recursión Estructural, Listas, Árboles y
+                 Definiciones Inductivas
+    Profesor: Dr. Marco Vladimir Lemus Yañez
+    Ayudante: Dr. Fernando Cruz Pineda
+    Fecha: 27/02/2026 
+
+    Inetgrantes del equipo:
+    - Becerra Valencia César
+    - Cortes Nava José Luis
+    - Díaz Anavia Javier Omar
+-}
+
+-- 4.1.Listas Definidas por el Usuario
+
+-- 4.2 Fold sobre Listas
+
+-- 4.3 Árboles Binarios
+
+-- 4.4 Estructuras Inductivas Lógicas
+
+
