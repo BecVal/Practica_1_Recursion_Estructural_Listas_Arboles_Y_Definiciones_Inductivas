14c2d098932d880f568cf15a85f5b9f85cbb6a57
Javier Diaz <javier.dz@ciencias.unam.mx>
2026-02-28 15:41:48 -0600

    Parte 4.2 y 4.3 hechas y documentadas

--- Practica_1.hs
+++ Practica_1.hs
@@ -1,4 +1,4 @@
-import Text.XHtml (rev)
+import Prelude hiding (zip)
 {-
     Práctica 1 - Recursión Estructural, Listas, Árboles y
                  Definiciones Inductivas
@@ -6,7 +6,7 @@ import Text.XHtml (rev)
     Ayudante: Dr. Fernando Cruz Pineda
     Fecha: 27/02/2026 
 
-    Inetgrantes del equipo:
+    Integrantes del equipo:
     - Becerra Valencia César
     - Cortes Nava José Luis
     - Díaz Anavia Javier Omar
@@ -14,7 +14,7 @@ import Text.XHtml (rev)
 
 -- 4.1 Listas Definidas por el Usuario
 
-data List a = Nil | Cons a (List a)
+data List a = Nil | Cons a (List a) deriving (Show)
 
 {-  
     Función reverseL: Toma una lista y devuelve una nueva lista con los elementos en orden inverso.
@@ -43,8 +43,80 @@ zip funcion (Cons elemento1 resto1) (Cons elemento2 resto2) =
 zip _ _ _ = Nil
 
 -- 4.2 Fold sobre Listas
+{-
+    Función foldrL: Reduce una lista a un solo valor, procesada de derecha a izquierda.
+    Caso base: Si la lista está vacía, regresamos el valor inicial 'z'.
+    Caso recursivo: Se aplica la función 'c' que combina el elemento actual 'x' con
+    el resultado acumulado de procesar el resto de la lista 'XS'.
+-}
+foldrL :: (a -> b -> b) -> b -> List a -> b
+foldrL _ z Nil = z
+foldrL c z (Cons x xs) = c x (foldrL c z xs)
+
+{-
+    Función lengthL: Te da la cantidad de elementos de una lista.
+    Usa foldrL con un acumulador que inicia en 0. Ignora el valor de cada elemento de la
+    lista y agrega 1 al acumulador.
+-}
+lengthL :: List a -> Int
+lengthL lista = foldrL sumarUno 0 lista
+  where
+    sumarUno _ acc = acc + 1
+
+{-
+    Función sumL: Suma todos los números enteros de una lista.
+    Usa foldrL con un valor inicial de 0. Utiliza el operador de suma (+)
+    para calcular la suma de cada elemento de la lista.
+-}
+sumL :: List Int -> Int
+sumL lista = foldrL (+) 0 lista
+
+{-
+    Función mapL: Transforma una lista con una función 'f' aplicada a cada elemento en la lista.
+    Usa foldrL iniciando con una lista vacía. Para cada elemento, aplica 'f' y usa
+    'Cons' para pegar el elemento transformado a la nueva lista.
+-}
+mapL :: (a -> b) -> List a -> List b
+mapL f lista = foldrL (Cons . f) Nil lista
 
 -- 4.3 Árboles Binarios
+data Tree a = 
+      Empty 
+    | Node a (Tree a) (Tree a) deriving (Show)
+
+{-
+    Función nhojas: Cuenta el número de hojas del árbol, que son los nodos sin hijos.
+    Caso base 1: Si el árbol está vacío, devuelve 0.
+    Caso base 2: Si el árbol es un solo nodo cuyos hijos izquierdo y derecho son vacíos,
+    devuelve 1. 
+    Caso recursivo: Suma la cantidad de hojas en el árbol izquierdo con las hojas del árbol
+    derecho.
+-}
+nhojas :: Tree a -> Integer
+nhojas Empty = 0
+nhojas (Node _ Empty Empty) = 1
+nhojas (Node _ a b) = nhojas a + nhojas b
+
+{-
+    Función inorder: Recorre el árbol en orden, con la secuencia Iz -> raíz -> Der.
+    Caso base: Si llegamos a una rama vacía, regresamos una lista vacía.
+    Caso recursivo: Concatenamos la lista resultante de la rama izquierda al nodo actual, y
+    La lista de la rama derecha.
+-}
+inorder :: Tree a -> [a]
+inorder Empty = []
+inorder (Node x a b) = inorder a ++ [x] ++ inorder b
+
+{-
+    Función dls: Búsqueda en profundidad para saber si un elemento está en el árbol o no.
+    Caso base: Si llegamos a un nodo vacío, el elemento no esta ahí. Regresamos False.
+    Case recursivo: Comprobamos si el nodo actual es igual 'y' es el que buscamos,
+    Si no, buscamos en la rama izquierda, si no lo encuentra, lo busca en la derecha.
+-}
+dfs :: Eq a => a -> Tree a -> Bool
+dfs _ Empty = False
+dfs x (Node y a b) =
+    (x == y) || dfs x a || dfs x b
 
 -- 4.4 Estructuras Inductivas Lógicas
 
