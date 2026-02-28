eaae72b9b1a5c3675199a4c8b53914d0c12c51e4
BecVal <cesarbecval@ciencias.unam.mx>
2026-02-28 14:44:41 -0600

    feat: Parte 4.1 implementada y documentada.

--- Practica_1.hs
+++ Practica_1.hs
@@ -12,7 +12,36 @@ import Text.XHtml (rev)
     - Díaz Anavia Javier Omar
 -}
 
--- 4.1.Listas Definidas por el Usuario
+-- 4.1 Listas Definidas por el Usuario
+
+data List a = Nil | Cons a (List a)
+
+{-  
+    Función reverseL: Toma una lista y devuelve una nueva lista con los elementos en orden inverso.
+    Caso base: Si la lista original ya está vacía, regresamos lo que juntamos.
+    Caso recursivo: Tomamos el primer elemento y lo ponemos al inicio del acumulador.
+    Luego, repetimos el proceso con el resto de la lista.
+-}
+reverseL :: List a -> List a
+reverseL listaInicial = auxiliar listaInicial Nil
+  where
+    auxiliar Nil acumulador = acumulador
+    auxiliar (Cons elemento resto) acumulador = auxiliar resto (Cons elemento acumulador)
+
+{-
+    Función zip: Toma dos listas del mismo tamaño y una función binaria, y devuelve una nueva lista
+    donde cada elemento es el resultado de aplicar la función a los elementos correspondientes de las dos listas.
+    Caso base: Si ambas listas están vacías, regresamos una lista vacía.
+    Caso recursivo: Si ambas listas tienen al menos un elemento.
+    Caso de seguridad: Por si acaso las listas llegan a ser de diferente tamaño, 
+    detenemos el proceso devolviendo Nil (el ejercicio dice que son del mismo tamaño, 
+    pero en Haskell siempre es buena práctica cubrir todos los casos).
+-}
+zip :: (a -> b -> c) -> List a -> List b -> List c
+zip funcion Nil Nil = Nil
+zip funcion (Cons elemento1 resto1) (Cons elemento2 resto2) = 
+    Cons (funcion elemento1 elemento2) (zip funcion resto1 resto2)
+zip _ _ _ = Nil
 
 -- 4.2 Fold sobre Listas
 
